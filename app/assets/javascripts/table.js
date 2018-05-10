14ab675206edeec6ada2fe5a21a06b78e9ea21b1
pratt0923 <snubly1@gmail.com>
2018-05-10 09:53:55 -0400

    highlight works

--- app/assets/javascripts/table.js
+++ app/assets/javascripts/table.js
@@ -2,7 +2,7 @@ function get_text_to_replace_p_tag(i, pg_section, pg_part) {
   var array = [];
   //split text
   var split_text = $(pg_section)[i].outerText.split('');
-
+  array.push("<p>");
   // put all text in array before query
   for (i = 0; i < pg_part.index; i++) {
     array.push(split_text[i]);
@@ -11,16 +11,21 @@ function get_text_to_replace_p_tag(i, pg_section, pg_part) {
   var split_query_length = pg_part[0].split('').length;
   var starting_point = pg_part.index + split_query_length;
 
-  array.push("<style class='highlight'>");
+  array.push("<span class='highlight'>");
   array.push(pg_part[0]);
-  array.push("</style>");
-  //need to put style string in here and then query and then end style string, all split
+  array.push("</span>");
+
   //put all text in array after query
-  for (i = 0; i > starting_point; i++) {
-    array.push(split_text[i]);
+  for (i = 0; i <= split_text.length; i++) {
+    if (i >= starting_point) {
+      array.push(split_text[i]);
+    };
   };
+
+  array.push("</p>");
   return Array.prototype.concat.apply([], array).join("");
 };
+
 
 //adding slashes to make the match work
 function sanitize_email_sections(part, section, pgsection, i) {
@@ -32,7 +37,6 @@ function sanitize_email_sections(part, section, pgsection, i) {
 // performting the match
   var pg_take_out = $(pgsection)[i].outerText.match(part);
   return get_text_to_replace_p_tag(i, pgsection, pg_take_out);
-  // return pg_take_out;
 }
 
 //making the padding for the pg and the email the same
@@ -44,7 +48,11 @@ for (i = 0; i < emails.length; i++) {
   var from_sanitized = $(".from")[i].value.split('');
   var subject_sanitized = $(".subject")[i].value.split('');
   var body_sanitized = $(".body")[i].value.split('');
-  var a = sanitize_email_sections(from_sanitized, ".from", ".pgfrom", i);
-  var b = sanitize_email_sections(subject_sanitized, ".subject", ".pgsubject", i);
-  var c = sanitize_email_sections(body_sanitized, ".body", ".pgbody", i);
+
+$('.pgfrom').eq(i).html(sanitize_email_sections(from_sanitized, ".from", ".pgfrom", i));
+$('.pgsubject').eq(i).html(sanitize_email_sections(subject_sanitized, ".subject", ".pgsubject", i));
+$('.pgbody').eq(i).html(sanitize_email_sections(body_sanitized, ".body", ".pgbody", i));
+  // var a = sanitize_email_sections(from_sanitized, ".from", ".pgfrom", i);
+  // var b = sanitize_email_sections(subject_sanitized, ".subject", ".pgsubject", i);
+  // var c = sanitize_email_sections(body_sanitized, ".body", ".pgbody", i);
 };
